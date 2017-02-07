496953b63dde3f3b736ca0062c20c19433183f6a
Alex Bleasdale <ableasdale@marklogic.com>
2017-02-07 04:56:48 +0000

    backing ut broken changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+QApplication a(argc, argv);
+qDebug() << "Hello World";
 
-    return a.exec();
+//int window_width = QApplication::desktop()->width();
+//int window_height = QApplication::desktop()->height();
+
+MainWindow w;
+
+/*
+QPushButton * quit_btn = new QPushButton;
+quit_btn->setParent(w.centralWidget());
+quit_btn->setGeometry(window_width-50,12,32,32);
+
+QObject::connect(quit_btn,SIGNAL(clicked()),qApp,SLOT(quit()));
+*/
+
+//w.resize(window_width,window_height);
+w.show();
+
+return a.exec();
 }
